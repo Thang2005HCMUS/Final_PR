2727abe739d960916282a600244a40c6af6ef76d
chettaou <chettaou@pc3369.igd.fraunhofer.de>
2025-07-29 11:21:45 +0200

    Revert baseline model loading to official dinov2 repo

--- src/backbone/model.py
+++ src/backbone/model.py
@@ -3,6 +3,7 @@ import logging
 import clip
 
 from transformers import Dinov2Model, Dinov2Config
+from .dinov2.models.vision_transformer import vit_small, vit_base, vit_large, vit_giant2
 
 class ClipModel():
     def __init__(self, rank, backbone_size):
@@ -19,78 +20,27 @@ class DINOv2Model():
         self.backbone.to(rank)
 
 
+
 class Dinov2BaselineModel():
-    def __init__(self, rank, backbone_size):
+    def __init__(self, rank, backbone_size, patch_size=14, init_values=0.1):
         logging.info("Loading scratch vit " + backbone_size + " ...")
 
+        backbone_archs = {
+            "small": "vits14",
+            "base": "vitb14",
+            "large": "vitl14",
+            "giant": "vitg14",
+        }
+
+        self.backbone_arch = backbone_archs[backbone_size]
+        self.backbone_name = f"dinov2_{self.backbone_arch}"
+
         if backbone_size == "small":
-            configuration = Dinov2Config(
-                hidden_size = 384,
-                num_hidden_layers = 12,
-                num_attention_heads = 6,
-                mlp_ratio = 4,
-                hidden_act = 'gelu',
-                layer_norm_eps = 1e-06,
-                image_size = 518,
-                patch_size = 14,
-                num_channels = 3,
-                qkv_bias = True,
-                layerscale_value = 1.0,
-                torch_dtype = torch.float32,
-                transformers_version = "4.31.0.dev0",
-            )
-
+            self.backbone = vit_small(patch_size=patch_size, init_values=init_values, block_chunks=False)
         elif backbone_size == "base":
-            configuration = Dinov2Config(
-                hidden_size = 768,
-                num_hidden_layers = 12,
-                num_attention_heads = 12,
-                mlp_ratio = 4,
-                hidden_act = 'gelu',
-                layer_norm_eps = 1e-06,
-                image_size = 518,
-                patch_size = 14,
-                num_channels = 3,
-                qkv_bias = True,
-                layerscale_value = 1.0,
-                torch_dtype = torch.float32,
-                transformers_version = "4.31.0.dev0",
-            )
-
+            self.backbone = vit_base(patch_size=patch_size, init_values=init_values, block_chunks=False)
         elif backbone_size == "large":
-            configuration = Dinov2Config(
-                hidden_size = 1024,
-                num_hidden_layers = 24,
-                num_attention_heads = 16,
-                mlp_ratio = 4,
-                hidden_act = 'gelu',
-                layer_norm_eps = 1e-06,
-                image_size = 518,
-                patch_size = 14,
-                num_channels = 3,
-                qkv_bias = True,
-                layerscale_value = 1.0,
-                torch_dtype = torch.float32,
-                transformers_version = "4.31.0.dev0",
-            )
+            self.backbone = vit_large(patch_size=patch_size, init_values=init_values, block_chunks=False)
         elif backbone_size == "giant":
-            configuration = Dinov2Config(
-                hidden_size = 1536,
-                num_hidden_layers = 40,
-                num_attention_heads = 24,
-                mlp_ratio = 4,
-                hidden_act = 'gelu',
-                layer_norm_eps = 1e-06,
-                image_size = 518,
-                patch_size = 14,
-                num_channels = 3,
-                qkv_bias = True,
-                layerscale_value = 1.0,
-                use_swiglu_ffn = True,
-                torch_dtype = torch.float32,
-                transformers_version = "4.31.0.dev0",
-            )
-
-        # Initializing a model (with random weights)
-        self.backbone = Dinov2Model(configuration)
+            self.backbone = vit_giant2(patch_size=patch_size, init_values=init_values, block_chunks=False)
         self.backbone.to(rank)
